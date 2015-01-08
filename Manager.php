57faf2864c85b18c0e8c66cf5edeab0424ac688a
xiewulong <xiewulong@vip.qq.com>
2015-01-08 16:43:36 +0800

    manager

--- Manager.php
+++ Manager.php
@@ -12,16 +12,18 @@
 namespace yii\qrcode;
 
 use Yii;
-use yii\base\ErrorException;
-use yii\helpers\Json;
+use PHPQRCode\QRcode;
 
 class Manager{
 
 	//文件名前缀
 	public $pre = 'qr_';
 
+	//缓存路径配置
+	public $temp = 'qrcode';
+
 	//存放路径
-	public $path = 'qrcode';
+	private $path = false;
 
 	//文件名
 	private $name = false;
@@ -35,89 +37,80 @@ class Manager{
 	//时间路径, 精确到天
 	public $timepath = false;
 
-	//错误纠正等级, 默认'L'
-	public $level;
+	//错误纠正等级0-3
+	public $level = 2;
 
-	//纠错等级取值范围
-	private $levelArr = [0, 1, 2, 3, 'l', 'm', 'q', 'h', 'L', 'M', 'Q', 'H'];
-
-	//纠错等级取值默认数组索引
-	private $levelDefaultIndex = 0;
-
-	//尺寸
+	//尺寸1-27
 	public $size = 4;
-
-	//最小尺寸
-	private $sizeMin = 1;
-
-	//最大尺寸
-	private $sizeMax = 10;
 
 	//周围间距, 正整数, 默认2 * 4(px)
 	public $margin = 2;
 
-	//背景颜色
-	public $bgcolor = 0xffffff;
-
-	//前景颜色
-	public $fgcolor = 0x000000;
+	//扩展名
+	private $ext = 'png';
 
 	/**
-	 * 构造器
-	 * @method __construct
+	 * 设置尺寸
+	 * @method setSize
 	 * @since 0.0.1
-	 * @return {none}
+	 * @param {number} $size 尺寸
+	 * @return {object}
+	 * @example Yii::$app->qrcode->setSize($size);
 	 */
-	public function __construct(){
+	public function setSize($size){
+		$this->size = $size;
+
+		return $this;
+	}
+
+	/**
+	 * 设置纠错等级
+	 * @method setLevel
+	 * @since 0.0.1
+	 * @param {string} $level 等级
+	 * @return {object}
+	 * @example Yii::$app->qrcode->setLevel($level);
+	 */
+	public function setLevel($level){
+		$this->level = $level;
+
+		return $this;
+	}
+
+	/**
+	 * 设置外间距
+	 * @method setMargin
+	 * @since 0.0.1
+	 * @param {number} $margin 外间距
+	 * @return {object}
+	 * @example Yii::$app->qrcode->setMargin($margin);
+	 */
+	public function setMargin($margin){
+		$this->margin = $margin;
+
+		return $this;
 	}
 
 	/**
 	 * 创建二维码
 	 * @method create
 	 * @since 0.0.1
+	 * @param {string} $data 数据
 	 * @return {array}
-	 * @example Yii::$app->qrcode->create($data, $ext);
+	 * @example Yii::$app->qrcode->create($data);
 	 */
-	public function create($data, $ext = 'png'){
-		echo(__DIR__ . '/phpqrcode/qrlib.php');
-		//$qrcode = QRcode::png;
-		switch($ext){
-			case 'png':
-				//echo 'png';
-				break;
-		}
+	public function create($data){
 		$path = $this->getPath();
-		$file = $this->getName() . '.' . $ext;
+		$file = $this->getName();
 		$root = $this->getRoot() . DIRECTORY_SEPARATOR . $file;
-		echo $root;
-		//$qrcode($data, $this->, $this->getLevel(), $this->getSize(), $this->margin, $this->bgcolor, $this->fgcolor);
-		
+		QRcode::png($data, $root, $this->level, $this->size, $this->margin);
+
 		return [
 			'name' => $this->name,
 			'path' => $path,
-			'root' => $file,
+			'root' => $root,
 			'url' => Yii::getAlias('@web/assets') . DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . $file,
 		];
-	}
-
-	/**
-	 * 获取尺寸
-	 * @method getSize
-	 * @since 0.0.1
-	 * @return {number}
-	 */
-	private function getSize(){
-		return min(max($this->size, $this->sizeMin), $this->sizeMax);
-	}
-
-	/**
-	 * 获取错误纠正等级
-	 * @method getLevel
-	 * @since 0.0.1
-	 * @return {string}
-	 */
-	private function getLevel(){
-		return in_array($this->level, $this->levelArr) ? $this->level : $this->levelArr[$this->levelDefaultIndex];
 	}
 
 	/**
@@ -128,7 +121,7 @@ class Manager{
 	 */
 	private function getName(){
 		if($this->name === false){
-			$this->name = $this->pre . $this->getTime() . '_' . mt_rand();
+			$this->name = $this->pre . $this->getTime() . '_' . md5(mt_rand()) . '.' . $this->ext;
 		}
 		return $this->name;
 	}
@@ -142,11 +135,10 @@ class Manager{
 	private function getRoot(){
 		if($this->root === false){
 			$this->root = Yii::getAlias('@webroot/assets') . DIRECTORY_SEPARATOR . $this->getPath();
+			if(!file_exists($this->root)){
+				mkdir($this->root, 0777, true);
+			}
 		}
-
-		if(!file_exists($this->root)){
-			mkdir($this->root, 0777, true);
-		};
 
 		return $this->root;
 	}
@@ -158,8 +150,11 @@ class Manager{
 	 * @return {string}
 	 */
 	private function getPath(){
-		if($this->timepath){
-			$this->path .= date('/Y/m/d', $this->getTime());
+		if($this->path === false){
+			$this->path = $this->temp;
+			if($this->timepath){
+				$this->path .= date('/Y/m/d', $this->getTime());
+			}
 		}
 
 		return $this->path;
